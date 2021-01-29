2ca1db3f81440ecc0d010a220a80ef0145051190
Hijiri Maruo <hjroath1993@gmail.com>
2021-01-29 14:58:22 -0800

    Assignment4 commit

--- pythonclubapp/admin.py
+++ pythonclubapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MeetingMinute, Meeting, Resource
 # Register your models here.
+admin.site.register(MeetingMinute)
+admin.site.register(Meeting)
+admin.site.register(Resource)
--- pythonclubapp/models.py
+++ pythonclubapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class MeetingMinute(models.Model):
+    meetingname=models.CharField(max_length=255)
+    minutesdescription=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.meetingname
+
+    class Meta:
+        db_table='meetingminute'
+
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetinglocation=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    meetingtype=models.ForeignKey(MeetingMinute, on_delete=models.DO_NOTHING)
+    dateentered=models.DateField()
+    meetingtime=models.TimeField()
+
+    def __str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table='meeting'
+    
+
+
+class Resource(models.Model):
+    name=models.CharField(max_length=255)
+    location=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    meeting=models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    date=models.DateField()
+    time=models.TimeField()
+    text=models.TextField()
+    url=models.URLField()
+    description=models.TextField() 
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='resource'

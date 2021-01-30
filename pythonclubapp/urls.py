833c192cb1922ac13b842301e247c73f46b1872d
Hijiri Maruo <hjroath1993@gmail.com>
2021-01-29 17:45:04 -0800

    Assignment6 commit

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('meetings/', views.meetings, name='meetings'),
     path('meetingminutes/', views.meetingminutes, name='meetingminutes'),
     path('resources/', views.resources, name='resources'),
+    path('meetingDetail/<int:id>', views.meetingDetail, name='detail'),
+    path('meetingminuteDetail/<int:id>', views.meetingminuteDetail, name='detail1'),
+    path('resourceDetail/<int:id>', views.resourceDetail, name='detail2'),
 ]
--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinute, Resource
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -17,4 +18,17 @@ def meetingminutes(request):
 
 def resources(request):
     resource_list = Resource.objects.all()
-    return render(request, 'pythonclubapp/resources.html', {'resource_list': resource_list})        
+    return render(request, 'pythonclubapp/resources.html', {'resource_list': resource_list})  
+
+def meetingDetail(request, id):
+    meeting = get_object_or_404(Meeting, pk = id)
+    return render(request, 'pythonclubapp/meetingdetail.html', {'meeting' : meeting})
+
+
+def meetingminuteDetail(request, id):
+    meetingminute = get_object_or_404(MeetingMinute, pk = id)
+    return render(request, 'pythonclubapp/meetingminutedetail.html', {'meetingminute' : meetingminute})    
+
+def resourceDetail(request, id):
+    resource = get_object_or_404(Resource, pk = id)
+    return render(request, 'pythonclubapp/resourcedetail.html', {'resource' : resource})             

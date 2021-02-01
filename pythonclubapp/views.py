0f0d2bf2159f59ecb71789351568994842f051a6
Hijiri Maruo <hjroath1993@gmail.com>
2021-01-31 20:57:34 -0800

    Assignment9 commit

--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -11,27 +11,33 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'pythonclubapp/index.html')
 
+@login_required
 def meetings(request):
     meeting_list = Meeting.objects.all()
     return render(request, 'pythonclubapp/meetings.html', {'meeting_list': meeting_list})
 
+@login_required
 def meetingminutes(request):
     meetingminute_list = MeetingMinute.objects.all()
     return render(request, 'pythonclubapp/meetingminutes.html', {'meetingminute_list': meetingminute_list})
 
+@login_required
 def resources(request):
     resource_list = Resource.objects.all()
     return render(request, 'pythonclubapp/resources.html', {'resource_list': resource_list})  
 
+
+@login_required
 def meetingDetail(request, id):
     meeting = get_object_or_404(Meeting, pk = id)
     return render(request, 'pythonclubapp/meetingdetail.html', {'meeting' : meeting})
 
-
+@login_required
 def meetingminuteDetail(request, id):
     meetingminute = get_object_or_404(MeetingMinute, pk = id)
     return render(request, 'pythonclubapp/meetingminutedetail.html', {'meetingminute' : meetingminute})    
 
+@login_required
 def resourceDetail(request, id):
     resource = get_object_or_404(Resource, pk = id)
     return render(request, 'pythonclubapp/resourcedetail.html', {'resource' : resource})             
@@ -52,6 +58,7 @@ def newMeeting(request):
         form = MeetingForm()
     return render(request, 'pythonclubapp/newmeeting.html', {'form': form})  
 
+@login_required
 def newMeetingMinute(request):
     form = MeetingMinuteForm
 
@@ -65,7 +72,7 @@ def newMeetingMinute(request):
         form = MeetingMinuteForm()
     return render(request, 'pythonclubapp/newmeetingminute.html', {'form': form})       
 
-
+@login_required
 def newResource(request):
     form = ResourceForm
 
